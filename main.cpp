d90d721086d840a6c8a053be8428d2bcee7e5170
Sofia <127309031+SofiZala@users.noreply.github.com>
2023-03-22 21:29:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,62 +2,61 @@
 #include <iostream>
 #include <cstring> 
 
-int main (void) {
+int main () {
+    std:: cout << "Would you like to insert data from file, from standard input or run a random simulation? [F,S,R]" << std::endl; 
+    char data;
+    std:: cin >> data;
+    while (data!='F' && data!='S' && data!='R') {
+        std:: cout << "Invalid character, try again." << std:: endl;
+        std:: cin >> data;
+    }
     
-    std::string control;
-    std::string control2;
+    switch (data) {
+        case 'F' : 
+            std::cout << "Qui ci andrà la simulazione da file, non l'ho ancora implementata XD"<< std::endl;
+            break;
 
-    std:: cout <<  "Do you want to insert your data or run a random simulation? (data/random)"; 
-    std:: cin >> control;
+        case 'S' :
+            std::cout << "Siamo in S" << std::endl;
+            /*People p;
+            p.setPeople();
+            Parameters ps;
+    
+            int Time;
+            int Num=p.getTotal();
+            std::cout << "Insert time simulation"<<std::endl;
+            std::cin>>Time;
+            sird oggetto(Time, p, ps, Num);
+            oggetto.simulate(); */
+            break;
 
-    if (control == "data"){
-        std:: cout <<  "Would you like to insert data from file o standard input? (file/std)"; 
-        std:: cin >> control2;
-
-        if (control2 == "file") {
-        std::cout << "Qui ci andrà la simulazione da file, non l'ho ancora implementata XD";
-           
-        } else {
-        //standard input simulation
-        People p;
-        p.setPeople();
-
-        Parameters ps;
+        case 'R' :
+            std::cout << "Siamo in R" << std::endl;
+            /*
+            //Random simulation elapse
+            int Time = 10+ (rand() % 100);
+            
+            //setting people
+            People p;
+            double s, i, r, d;
+            s= rand() % 5501 + 500; //suscettibili tra 500 e 6000  
+            i= rand() % 101; //infetti tra 0 e 100
+            r=rand() % 501;  //resuscitati tra 0 e 500
+            d=rand() % 201; //deceduti tra 0 e 200
+            std:: cout << "Your random values are:" << std:: endl <<
+                "Susceptible = " << s << std::endl << "Infected = " << i << std::endl << 
+                "Recovered = " << r << std::endl << "Dead = " << d << std::endl; 
+            p.setPeople(s, i, r, d);
+            int Num=p.getTotal();
         
-        int Time;
-        int Num=p.getTotal();
-        std::cout << "Insert time simulation"<<std::endl;
-        std::cin>>Time;
-        sird oggetto(Time, p, ps, Num);
-        oggetto.simulate();
+            Parameters ps;
 
-        }
-    } else {
-        //Random simulation elapse
-        int Time = 10+ (rand() % 100);
-        
-        //setting people
-        People p;
-        double s, i, r, d;
-        s= rand() % 5501 + 500; //suscettibili tra 500 e 6000  
-        i= rand() % 101; //infetti tra 0 e 100
-        r=rand() % 501;  //resuscitati tra 0 e 500
-        d=rand() % 201; //deceduti tra 0 e 200
-        std:: cout << "Your random values are:" << std:: endl <<
-               "Susceptible = " << s << std::endl << "Infected = " << i << std::endl << 
-               "Recovered = " << r << std::endl << "Dead = " << d << std::endl; 
-        p.setPeople(s, i, r, d);
-        int Num=p.getTotal();
-       
-       Parameters ps;
+            sird oggetto(Time, p, ps, Num);
+            oggetto.simulate(); */
+            break;
 
-       sird oggetto(Time, p, ps, Num);
-       oggetto.simulate();
-        
-
-
-        //analogo per i parametri, da implementare
-}
+        // default: std:: cout << "Invalid character.";  NON PENSO SERVA XKE NON DOVREBBE MAI ARRIVARE QUI
+    }
 }
 
 
