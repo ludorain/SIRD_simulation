bdb5cf53e87f75e877d3811612d82b4e7d8a5719
ludorain <127125395+ludorain@users.noreply.github.com>
2023-03-22 14:51:57 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "sird.hpp"
+#include <iostream>
+#include <cstring> 
+
+int main (void) {
+    
+    std::string control;
+    std::string control2;
+
+    std:: cout <<  "Do you want to insert your data or run a random simulation? (data/random)"; 
+    std:: cin >> control;
+
+    if (control == "data"){
+        std:: cout <<  "Would you like to insert data from file o standard input? (file/std)"; 
+        std:: cin >> control2;
+
+        if (control2 == "file") {
+        std::cout << "Qui ci andrà la simulazione da file, non l'ho ancora implementata XD";
+           
+        } else {
+        //standard input simulation
+        People p;
+        p.setPeople();
+
+        Parameters ps;
+        
+        int Time;
+        int Num=p.getTotal();
+        std::cout << "Insert time simulation"<<std::endl;
+        std::cin>>Time;
+        sird(Time, p, ps, Num);
+        }
+    } else {
+        //Random simulation elapse
+        int Time = 10+ (rand() % 100);
+        
+        //setting people
+        People p;
+        double s, i, r, d;
+        s=1+ (rand() % 6000);
+        i=1+ (rand() % 10);
+        r=1+ (rand() % 100);
+        d=1+ (rand() % 100);
+        p.setPeopleR(s, i, r, d);
+        int Num=p.getTotal();
+
+        //analogo per i parametri, da implementare
+}
+}
