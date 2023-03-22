aeab43ca46c122d12731a5315696befb5a3dd980
ludorain <127125395+ludorain@users.noreply.github.com>
2023-03-22 14:50:52 +0100

    Update sird.hpp

--- sird.hpp
+++ sird.hpp
@@ -1,45 +1,73 @@
 #ifndef SIRD_CPP
 #define SIRD_CPP
 
-namespace model {
+/*IDEA PROGRAMMA
+struct People
+struct Parametri
 
-struct People{
-    double S{};
-    double I{};
-    double R{};
-    double D{};
-}
+costruttore con dati default
+
+setter parametri con verifica range
+
+getter parametri
+
+funzione simulate (può essere messa nel main, che vantaggi?)
+*/
+
+
+class People{
+friend class sird;    
+public:
+//costruttore default
+People();
+//funzioni set
+void setSusceptible();
+void setInfected();
+void setRecovered();
+void setDeads();
+void setPeople();
+void setPeople(double, double, double, double);
+
+//funzioni get
+int getTotal();
+
+//Da scrivere
+private:
+
+    static double S;
+    static double I;
+    static double R;
+    static double D;
+};
+
+class Parameters{
+friend class sird;
+//costruttore non attuale, è solo per fare delle prove
+public:
+Parameters(double=0.0001, double=0.7, double=0.3, double=0.1);
+
+private: 
+static double alfa;
+static double beta;
+static double gamma;
+static double mu;
+};
 
 class sird{
 
-People people;
-//Time, total people, probability of infection, probability of recovery, probability
-int T;
-double N;
-double beta;
-double gamma;
-double alfa;
-double mu;
-
 public:
-sird(double T_, double alfa_, double beta_, double gamma_, double mu_, People people_ );
-//getter parametri
-double alfa();
-double beta();
-double alfa();
-double mu();
-int T();
-int people(); //verificare
-
+sird(int T_, People people_, Parameters parameters_, int N_);
+void simulate();
 
 //mancano  i metodi
 
+private:
 
-
-
-
-
-}
-
-}
+//People people;
+//Time, total people, probability of infection, probability of recovery, probability
+int T;
+People people;
+Parameters parameters;
+int N;
+};
 #endif
