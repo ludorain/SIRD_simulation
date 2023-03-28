924a8455833eb113866ace4ea4723b6941e8aeab
Ludovica Rainero <ludovica.rainero@studio.unibo.it>
2023-03-28 09:56:10 +0200

    28-03

--- versione1/main.cpp
+++ versione1/main.cpp
@@ -1,165 +1,174 @@
-#include "sird.hpp"
+#include <cmath>
+#include <cstring>  //mi serve  per line
+#include <ctime>    // ci serve per la funzione time che sta dentro al random
+#include <fstream>  //serve per usare ifs
 #include <iostream>
-#include <ctime> // ci serve per la funzione time che sta dentro al random
-#include <cmath> 
-#include <cstring> //mi serve  per line
-#include <fstream> //serve per usare ifs
 #include <typeinfo>
 
+#include "sird.hpp"
 
-//random generation prototype function
+// random generation prototype function
 double fRand(double, double);
-double controlTime();
+int controlTime();
 
-int main () {
+int main() {
+  std::cout << "Would you like to insert data from file, from standard input "
+               "or run a random simulation? [F,S,R]"
+            << std::endl;
+  char data;
+  std::cin >> data;
 
-std:: cout << "Would you like to insert data from file, from standard input or run a random simulation? [F,S,R]" << std::endl; 
-char data;
-std:: cin >> data;
+  while (data != 'F' && data != 'S' && data != 'R') {
+    std::cout << "Invalid character, try again." << std::endl;
+    std::cin >> data;
+  }
 
-while (data!='F' && data!='S' && data!='R') {
-std:: cout << "Invalid character, try again." << std:: endl;
-std:: cin >> data; 
-}
-    
-if (data == 'F') {
+  if (data == 'F') {
+    std::cout << "Great, you've choosen ..." << std::endl;
 
-    std::cout << "Great, you've choosen ..."<< std::endl;
-    
     std::string line;
     std::ifstream ifs;
     ifs.open("Data.txt");
-    double s,i,r,d;
-    double a,b,g,u;
-    int Time; 
+    double s, i, r, d;
+    double a, b, g, u;
+    int Time;
 
-    getline (ifs,line,'.');
-    getline (ifs,line, ':');
-    getline (ifs,line,' ');
+    getline(ifs, line, '.');
+    getline(ifs, line, ':');
+    getline(ifs, line, ' ');
     ifs >> s;
-    getline (ifs,line,' ');
+    getline(ifs, line, ' ');
     ifs >> i;
-    getline (ifs,line,' ');
-    ifs >> r,
-    getline (ifs,line,' ');
+    getline(ifs, line, ' ');
+    ifs >> r, getline(ifs, line, ' ');
     ifs >> d;
-    getline (ifs,line,':');
-    getline (ifs,line,' ');
+    getline(ifs, line, ':');
+    getline(ifs, line, ' ');
     ifs >> a;
-    getline (ifs,line,' ');
+    getline(ifs, line, ' ');
     ifs >> b;
-    getline (ifs,line,' ');
-    ifs >> g,
-    getline (ifs,line,' ');
+    getline(ifs, line, ' ');
+    ifs >> g, getline(ifs, line, ' ');
     ifs >> u;
-    getline (ifs,line,' ');
+    getline(ifs, line, ' ');
     ifs >> Time;
 
-    std:: cout << a << std:: endl << b << std:: endl << g << std:: endl << u << std:: endl;
-    
+    std::cout << a << std::endl
+              << b << std::endl
+              << g << std::endl
+              << u << std::endl;
+
     People p;
     Parameters ps;
     p.setPeople(s, i, r, d);
-    int Num=p.getTotal();
-    ps.setParameters(a,b,g,u); 
+    int Num = p.getTotal();
+    ps.setParameters(a, b, g, u);
 
     sird oggetto(Time, p, ps, Num);
-    oggetto.simulate(); //DATO CHE L'OGGETTO SIMULATE è UGUALE PER TUTTI NON POSSIAMO CHIAMARLO FUORI DALL'IF? SE HA SENSO PER ME POTREMMO CHIAMARE FUORI DALL'IF TUTTO CIò CHE SI CHIAMA NELLO STESSO MODO E CHE è INDIPENDENTE DALLA SCELTA INZIIALE SUI DATI
+    oggetto.simulate();  // DATO CHE L'OGGETTO SIMULATE è UGUALE PER TUTTI NON
+                         // POSSIAMO CHIAMARLO FUORI DALL'IF? SE HA SENSO PER ME
+                         // POTREMMO CHIAMARE FUORI DALL'IF TUTTO CIò CHE SI
+                         // CHIAMA NELLO STESSO MODO E CHE è INDIPENDENTE DALLA
+                         // SCELTA INZIIALE SUI DATI
 
-} else if (data == 'S') {
+  } else if (data == 'S') {
+    std::cout << "Great, you've choosen Standard Input, please insert "
+              << std::endl;
 
-    std::cout << "Great, you've choosen Standard Input, please insert " << std::endl;
-    
-    //Costructor calls
+    // Costructor calls
     People p;
     p.setPeople();
-    int Num=p.getTotal();
+    int Num = p.getTotal();
 
     Parameters ps;
     ps.setParameters();
 
-    int Time=controlTime();    
-    
-    sird oggetto(Time, p, ps, Num);
-    oggetto.simulate(); 
+    int Time = controlTime();
 
-} else if (data == 'R') {
-    std::cout << "Great, you've choosen Random generation, here's your datas" << std::endl;
-    
-    //Generate random seed for rand()
-    srand(time(NULL));
-
-    //Creating objects 
-    People p;
-    Parameters ps;
-
-    //Time elapse
-    int Time = 30+ (rand() % 101);
-
-    //Generate people for simulation
-    double s=rand() % 500 + 5501; //suscettibili tra 500 e 6000   5000-10000
-    double i=rand() % 101; //infetti tra 0 e 100
-    double r=rand() % 501;  //resuscitati tra 0 e 500
-    double d=rand() % 101; //deceduti tra 0 e 100
-
-    //Generate parameters for simulation
-    char f;
-    std::cout<<"Do you want vaccination? [y/n]";
-    std::cin>>f;
-    while(f!='y' && f!='n'){
-    std::cout<<"Invalid char, try again.";
-    std::cin>>f; }
-    double a;
-
-    if(f=='y')
-    { a= fRand(0.001, 0.01); //between 0.001 e 0.01
-    
-    } else {a=0;}
-    
-    double b=fRand(0.100, 0.900); //between 0.1 e 0.9
-    double g=fRand(0.100, 0.500); //
-    double u=fRand(0.100, 0.500);
-
-    // Assign random values to class object and print them
-    p.setPeopleR(s, i, r, d);
-    int Num=p.getTotal();
-    ps.setParametersR(a,b,g,u);
-
-    std::cout << "The initial persons are:" << std:: endl;
-    std::cout << "Susceptible = " << s << std::endl << "Infected = " << i << std::endl;
-    std::cout << "Recovered = " << r << std::endl << "Dead = " << d << std::endl;   
-    std::cout << "Total people =" << Num << std::endl;
-
-    std::cout << "The initial parameters are:" << std:: endl;
-    std::cout << "alfa = " << a << std::endl << "beta = " << b << std::endl;
-    std::cout << "gamma = " << g << std::endl << "mu = " << u << std::endl;
-    std::cout << "Simulation time= " << Time <<std::endl;
-
-    //Starting proper simulation
     sird oggetto(Time, p, ps, Num);
     oggetto.simulate();
 
-}
+  } else if (data == 'R') {
+    std::cout << "Great, you've choosen Random generation, here's your datas"
+              << std::endl;
+
+    // Generate random seed for rand()
+    srand(time(NULL));
+
+    // Creating objects
+    People p;
+    Parameters ps;
+
+    // Time elapse
+    int Time = 30 + (rand() % 101);
+
+    // Generate people for simulation
+    double s = rand() % 500 + 5501;  // suscettibili tra 500 e 6000   5000-10000
+    double i = rand() % 101;         // infetti tra 0 e 100
+    double r = rand() % 501;         // resuscitati tra 0 e 500
+    double d = rand() % 101;         // deceduti tra 0 e 100
+
+    // Generate parameters for simulation
+    char f;
+    std::cout << "Do you want vaccination? [y/n]";
+    std::cin >> f;
+    while (f != 'y' && f != 'n') {
+      std::cout << "Invalid char, try again.";
+      std::cin >> f;
+    }
+    double a;
+
+    if (f == 'y') {
+      a = fRand(0.001, 0.01);  // between 0.001 e 0.01
+
+    } else {
+      a = 0;
+    }
+
+    double b = fRand(0.100, 0.900);  // between 0.1 e 0.9
+    double g = fRand(0.100, 0.500);  //
+    double u = fRand(0.100, 0.500);
+
+    // Assign random values to class object and print them
+    p.setPeopleR(s, i, r, d);
+    int Num = p.getTotal();
+    ps.setParametersR(a, b, g, u);
+
+    std::cout << "The initial persons are:" << std::endl;
+    std::cout << "Susceptible = " << s << std::endl
+              << "Infected = " << i << std::endl;
+    std::cout << "Recovered = " << r << std::endl
+              << "Dead = " << d << std::endl;
+    std::cout << "Total people =" << Num << std::endl;
+
+    std::cout << "The initial parameters are:" << std::endl;
+    std::cout << "alfa = " << a << std::endl << "beta = " << b << std::endl;
+    std::cout << "gamma = " << g << std::endl << "mu = " << u << std::endl;
+    std::cout << "Simulation time= " << Time << std::endl;
+
+    // Starting proper simulation
+    sird oggetto(Time, p, ps, Num);
+    oggetto.simulate();
+  }
 }
 
-double fRand(double fMin, double fMax)
-{
-    double f = (double)rand() / RAND_MAX;
-    return fMin + f * (fMax - fMin);
+double fRand(double fMin, double fMax) {
+  double f = (double)rand() / RAND_MAX;
+  return fMin + f * (fMax - fMin);
 }
 
-double controlTime(){
+int controlTime() {
+  int t;
+  double fractPart, intPart;
 
-double t;
-double fractPart, intPart;
-    
-    std::cout << "Insert time simulation"<<std::endl;
-    std::cin>>t;
-   
-    fractPart = modf(t, &intPart);
-    while(t<1 || (typeid(t).name()!="i" && fractPart!=0.00)){
-    std::cout<<"Time parameter out of range. Enter a new value"<<std::endl;
-    std::cin>>t;}
-    
-    return t;
+  std::cout << "Insert time simulation" << std::endl;
+  std::cin >> t;
+
+  fractPart = modf(t, &intPart);
+  while (t < 1 || (typeid(t).name() != "i" && std::abs(fractPart) > 1e-9)) {
+    std::cout << "Time parameter out of range. Enter a new value" << std::endl;
+    std::cin >> t;
+  }
+
+  return t;
 }
--- versione1/sird.cpp
+++ versione1/sird.cpp
@@ -1,282 +1,278 @@
 #include "sird.hpp"
+
 #include <cmath>
 #include <iostream>
 #include <typeinfo>
 
-People::People()
-{ }
+People::People() {}
 
-//setter class People
-void People::setSusceptible() 
-{
-    double s, fractPart, intPart;
-    std::cout<<"The number of initial Susceptible [range 0-60000]" << std::endl;
-    std::cin>>s;
-    fractPart = modf(s, &intPart);
-    while(s<1 || s>60000 || (typeid(s).name()!="i" && fractPart!=0.00)){
-    std::cout<<"susceptible parameter out of range. Enter a new value"<<std::endl;
-    std::cin>>s;}
-    S=s;
-    }
-void People::setInfected()
-{
-    double i, fractPart, intPart;
-    std::cout<<"The number of initial Infected [range 0-6000]" << std::endl;
-    std::cin>>i;
-    fractPart = modf(i, &intPart);
-    while(i<1 || i>6000 || (typeid(i).name()!="i" && fractPart!=0.00)){
-    std::cout<<"Infected parameter out of range. Enter a new value"<<std::endl;
-    std::cin>>i;}
-    I=i;
-    }
-void People::setRecovered()
-{
-    double r, fractPart, intPart;
-    std::cout<<"The number of initial Recovered [range 0-6000]" << std::endl;
-    std::cin>>r;
-    fractPart = modf(r, &intPart);
-    while(r<0 || r>6000 || (typeid(r).name()!="i" && fractPart!=0.00)){
-    std::cout<<"Recovered parameter out of range. Enter a new value"<<std::endl;
-    std::cin>>r;}
-    R=r;
-    }
-void People::setDeads()
-{
-    double d, fractPart, intPart;
-    std::cout<<"The number of initial Deads [range 0-6000]" << std::endl;
-    std::cin>>d;
-    fractPart = modf(d, &intPart);
-    while(d<0 || d>6000 || (typeid(d).name()!="i" && fractPart!=0.00)){
-    std::cout<<"susceptible parameter out of range. Enter a new value"<<std::endl;
-    std::cin>>d;}
-    D=d;
-    }
-
-void People::setPeople(){
-    setSusceptible();
-    setInfected();
-    setRecovered();
-    setDeads();
+// setter class People
+void People::setSusceptible() {
+  double s, fractPart, intPart;
+  std::cout << "The number of initial Susceptible [range 0-60000]" << std::endl;
+  std::cin >> s;
+  fractPart = modf(s, &intPart);
+  while (s < 1 || s > 60000 || (typeid(s).name() != "i" && fractPart != 0.00)) {
+    std::cout << "susceptible parameter out of range. Enter a new value"
+              << std::endl;
+    std::cin >> s;
+  }
+  S = s;
 }
-//setPeople() function overloading 
-
-//function for file input
-void People::setPeople(double s, double i, double r, double d){
-double fractPart, intPart;
-//susceptible control
-    fractPart = modf(s, &intPart);
-    if(s>0 && s<6000 && (typeid(s).name()!="i" || fractPart!=0.00)){
-        S=s;
-    } else {std::cout<<"S Parameter out of range, initialized to default value";}
-    
-//infected control
-    fractPart = modf(i, &intPart);    
-    if(i>0 && i<6000 && (typeid(i).name()!="i" || fractPart!=0.00)){
-        I=i;
-    } else {std::cout<<"I Parameter out of range, initialized to default value";}
-
-//recovered control
-    fractPart = modf(r, &intPart);
-    if(r>0 && r<6000 && (typeid(r).name()!="i" || fractPart!=0.00)){
-        R=r;
-    } else {std::cout<<"R Parameter out of range, initialized to default value";}
-
-//deaths control    
-    fractPart = modf(d, &intPart);
-    if(d>0 && d<6000 && (typeid(d).name()!="i" || fractPart!=0.00)){
-        D=d;
-    } else {std::cout<<"D Parameter out of range, initialized to default value";}
+void People::setInfected() {
+  double i, fractPart, intPart;
+  std::cout << "The number of initial Infected [range 0-6000]" << std::endl;
+  std::cin >> i;
+  fractPart = modf(i, &intPart);
+  while (i < 1 || i > 6000 || (typeid(i).name() != "i" && fractPart != 0.00)) {
+    std::cout << "Infected parameter out of range. Enter a new value"
+              << std::endl;
+    std::cin >> i;
+  }
+  I = i;
+}
+void People::setRecovered() {
+  double r, fractPart, intPart;
+  std::cout << "The number of initial Recovered [range 0-6000]" << std::endl;
+  std::cin >> r;
+  fractPart = modf(r, &intPart);
+  while (r < 0 || r > 6000 || (typeid(r).name() != "i" && fractPart != 0.00)) {
+    std::cout << "Recovered parameter out of range. Enter a new value"
+              << std::endl;
+    std::cin >> r;
+  }
+  R = r;
+}
+void People::setDeads() {
+  double d, fractPart, intPart;
+  std::cout << "The number of initial Deads [range 0-6000]" << std::endl;
+  std::cin >> d;
+  fractPart = modf(d, &intPart);
+  while (d < 0 || d > 6000 || (typeid(d).name() != "i" && fractPart != 0.00)) {
+    std::cout << "susceptible parameter out of range. Enter a new value"
+              << std::endl;
+    std::cin >> d;
+  }
+  D = d;
 }
 
-//function for random input
-void People::setPeopleR(double s, double i, double r, double d){
-    S=s;
-    I=i;
-    R=r;
-    D=d;
+void People::setPeople() {
+  setSusceptible();
+  setInfected();
+  setRecovered();
+  setDeads();
+}
+// setPeople() function overloading
+
+// function for file input
+void People::setPeople(double s, double i, double r, double d) {
+  double fractPart, intPart;
+  // susceptible control
+  fractPart = modf(s, &intPart);
+  if (s > 0 && s < 6000 && (typeid(s).name() != "i" || fractPart != 0.00)) {
+    S = s;
+  } else {
+    std::cout << "S Parameter out of range, initialized to default value";
+  }
+
+  // infected control
+  fractPart = modf(i, &intPart);
+  if (i > 0 && i < 6000 && (typeid(i).name() != "i" || fractPart != 0.00)) {
+    I = i;
+  } else {
+    std::cout << "I Parameter out of range, initialized to default value";
+  }
+
+  // recovered control
+  fractPart = modf(r, &intPart);
+  if (r > 0 && r < 6000 && (typeid(r).name() != "i" || fractPart != 0.00)) {
+    R = r;
+  } else {
+    std::cout << "R Parameter out of range, initialized to default value";
+  }
+
+  // deaths control
+  fractPart = modf(d, &intPart);
+  if (d > 0 && d < 6000 && (typeid(d).name() != "i" || fractPart != 0.00)) {
+    D = d;
+  } else {
+    std::cout << "D Parameter out of range, initialized to default value";
+  }
 }
 
-//getter class People
-int People::getTotal(){
-    int Tot=S+I+R+D;
-    return Tot;
+// function for random input
+void People::setPeopleR(double s, double i, double r, double d) {
+  S = s;
+  I = i;
+  R = r;
+  D = d;
 }
-double People::getSusceptible(){
-    return S;
-};
-double People::getInfected(){
-    return I;
-};
-double People::getRecovered(){
-    return R;
-};
-double People::getDeads(){
-    return D;
-};
 
+// getter class People
+int People::getTotal() {
+  int Tot = S + I + R + D;
+  return Tot;
+}
+double People::getSusceptible() { return S; };
+double People::getInfected() { return I; };
+double People::getRecovered() { return R; };
+double People::getDeads() { return D; };
 
-//costruttore
-Parameters::Parameters()
-{ }
+// costruttore
+Parameters::Parameters() {}
 
-//setter della classe Parameters
+// setter della classe Parameters
 
-//Setter per lo standard input
-void Parameters::setAlfa() 
-{
-    char f;
-    std::cout<<"Do you want vaccination? [y/n]";
-    std::cin>>f;
-    while(f!='y' && f!='n'){
-    std::cout<<"Invalid char, try again.";
-    std::cin>>f; }
+// Setter per lo standard input
+void Parameters::setAlfa() {
+  char f;
+  std::cout << "Do you want vaccination? [y/n]";
+  std::cin >> f;
+  while (f != 'y' && f != 'n') {
+    std::cout << "Invalid char, try again.";
+    std::cin >> f;
+  }
 
-    if(f=='y')
-    { double a;
-    std::cout<<"Rate vaccination [range 0.001-0.01]" << std::endl;
-    std::cin>>a;
-    while(a<=0.001 || a>=0.01){
-    std::cout<<"out of range. Enter a new value"<<std::endl;
-    std::cin>>a;}
-    alfa=a;
-    } else {alfa=0;}
-    
-    
+  if (f == 'y') {
+    double a;
+    std::cout << "Rate vaccination [range 0.001-0.01]" << std::endl;
+    std::cin >> a;
+    while (a <= 0.001 || a >= 0.01) {
+      std::cout << "out of range. Enter a new value" << std::endl;
+      std::cin >> a;
+    }
+    alfa = a;
+  } else {
+    alfa = 0;
+  }
+}
+
+void Parameters::setBeta() {
+  double b;
+  std::cout << "Infection probability [range 0-1]" << std::endl;
+  std::cin >> b;
+  while (b < 0 || b > 1) {
+    std::cout << " out of range. Enter a new value" << std::endl;
+    std::cin >> b;
+  }
+  beta = b;
+}
+
+void Parameters::setGamma() {
+  double g;
+  std::cout << "Recovery probability [range 0-0.5]" << std::endl;
+  std::cin >> g;
+  while (g < 0 || g > 0.5) {
+    std::cout << "out of range. Enter a new value" << std::endl;
+    std::cin >> g;
+  }
+  gamma = g;
+}
+void Parameters::setMu() {
+  double u;
+  std::cout << "Death probability [range 0-0.5]" << std::endl;
+  std::cin >> u;
+  while (u < 0 || u > 0.5) {
+    std::cout << "susceptible parameter out of range. Enter a new value"
+              << std::endl;
+    std::cin >> u;
+  }
+  mu = u;
+}
+
+void Parameters::setParameters() {
+  setAlfa();
+  setBeta();
+  setGamma();
+  setMu();
+}
+// setParameters() function overloading ,
+// function for file input
+void Parameters::setParameters(double a, double b, double g, double u) {
+  if (a >= 0 && a <= 0.01) {
+    alfa = a;
+  } else {
+    std::cout << "Parameter out of range, initialized to default value a";
+  }
+  if (b > 0 && b < 1) {
+    beta = b;
+  } else {
+    std::cout << "Parameter out of range, initilized to default value b";
+  }
+
+  if (g > 0 && g < 0.5) {
+    gamma = g;
+  } else {
+    std::cout << "Parameter out of range, initialized to default value g";
+  }
+
+  if (u > 0 && u < 0.5) {
+    mu = u;
+  } else {
+    std::cout << "Parameter out of range, initialized to default value u";
+  }
+}
+// function for random input
+void Parameters::setParametersR(double a, double b, double g, double u) {
+  alfa = a;
+  beta = b;
+  gamma = g;
+  mu = u;
+}
+
+// getter della classe Parameters
+double Parameters::getAlfa() { return alfa; };
+double Parameters::getBeta() { return beta; };
+double Parameters::getGamma() { return gamma; };
+double Parameters::getMu() { return mu; };
+
+// classe complessiva, si occupa della simulazione
+sird::sird(int T_, People people_, Parameters parameters_, int N_)
+    : T(T_),
+      people(people_),
+      parameters(parameters_),
+      N(N_) {  // controllo del parametro tempo
+}
+
+void sird::simulate() {
+  double const S0 = people.getSusceptible();
+
+  double s, s_ = people.getSusceptible();
+  double i, i_ = people.getInfected();
+  double r, r_ = people.getRecovered();
+  double d, d_ = people.getDeads();
+
+  // double s_= people.getSusceptible();
+  // double i_= people.getInfected();
+  // double r_= people.getRecovered();
+  // double d_= people.getDeads();
+
+  double a = parameters.getAlfa();
+  double b = parameters.getBeta();
+  double g = parameters.getGamma();
+  double m = parameters.getMu();
+
+  double t = T;
+
+  for (int j = 0; j < t; j++) {
+    s_ += ((-s * i * b) / ((double)N) - a * S0);
+    i_ += ((b * s * i / ((double)N)) - g * i - m * i);
+    d_ += m * i;
+    // s=-9, i=0, a*so+s
+    if (s_ > 0) {
+      r_ += (g * i + a * S0);
+    } else {
+      s_ = 0;
+      r_ += g * i + s;  //-(s2-s1)
     }
 
-void Parameters::setBeta()
-{
-    double b;
-    std::cout<<"Infection probability [range 0-1]" << std::endl;
-    std::cin>>b;
-    while(b<0 || b>1){
-    std::cout<<" out of range. Enter a new value"<<std::endl;
-    std::cin>>b;}
-    beta=b;
-    }
+    std::cout << static_cast<int>(s_) << "||" << static_cast<int>(i_) << "||"
+              << static_cast<int>(r_) << "||" << static_cast<int>(d_) << "||"
+              << static_cast<int>(s_ + i_ + r_ + d_) << std::endl;
 
-void Parameters::setGamma()
-{
-    double g;
-    std::cout<<"Recovery probability [range 0-0.5]" << std::endl;
-    std::cin>>g;
-    while(g<0 || g>0.5){
-    std::cout<<"out of range. Enter a new value"<<std::endl;
-    std::cin>>g;}
-    gamma=g;
-    }
-void Parameters::setMu()
-{
-    double u;
-    std::cout<<"Death probability [range 0-0.5]" << std::endl;
-    std::cin>>u;
-    while(u<0 || u>0.5){
-    std::cout<<"susceptible parameter out of range. Enter a new value"<<std::endl;
-    std::cin>>u;}
-    mu=u;
-    }
-
-void Parameters::setParameters(){
-    setAlfa();
-    setBeta();
-    setGamma();
-    setMu();
+    s = s_;
+    i = i_;
+    r = r_;
+    d = d_;
+  }
 }
-//setParameters() function overloading , 
-//function for file input
-void Parameters::setParameters(double a, double b, double g, double u){
-    if(a>=0 && a<=0.01){
-        alfa=a;
-    } else {std::cout<<"Parameter out of range, initialized to default value a";}
-    if(b>0 && b<1){
-        beta=b;
-    } else {std::cout<<"Parameter out of range, initilized to default value b";}
-
-    if(g>0&&g<0.5){
-        gamma=g;
-    } else {std::cout<<"Parameter out of range, initialized to default value g";}
-
-    if(u>0&&u<0.5){
-        mu=u;
-    } else {std::cout<<"Parameter out of range, initialized to default value u";}
-}
-//function for random input
-void Parameters::setParametersR(double a, double b, double g, double u){
-    alfa=a;
-    beta=b;
-    gamma=g;
-    mu=u;
-}
-
-//getter della classe Parameters
-double Parameters::getAlfa(){
-    return alfa;
-};
-double Parameters::getBeta(){
-    return beta;
-};
-double Parameters::getGamma(){
-    return gamma;
-};
-double Parameters::getMu(){
-    return mu;
-};
-
-//classe complessiva, si occupa della simulazione
-sird::sird(int T_, People people_, Parameters parameters_, int N_):
-    T(T_), people(people_), parameters(parameters_), N(N_)     
-{  //controllo del parametro tempo
-}
-
-
-void sird::simulate(){
-    double const S0=people.getSusceptible();
-    
-    double s,s_= people.getSusceptible();
-    double i,i_= people.getInfected();
-    double r,r_= people.getRecovered();
-    double d,d_= people.getDeads();
-
-    //double s_= people.getSusceptible();
-    //double i_= people.getInfected();
-    //double r_= people.getRecovered();
-    //double d_= people.getDeads();
-
-
-    double a= parameters.getAlfa();
-    double b= parameters.getBeta();
-    double g= parameters.getGamma();
-    double m= parameters.getMu();
-
-    double t=T;
-
-    for(int j=0; j<t; j++){
-        
-        s_+=((-s*i*b)/((double)N) - a*S0);
-        i_+=((b*s*i/((double)N))-g*i-m*i);        
-        d_+=m*i;
-        // s=-9, i=0, a*so+s 
-        if (s_>0) {
-            r_+=(g*i+a*S0);
-        } else {
-            s_=0;
-            r_+=g*i +s; //-(s2-s1)
-        }
-        
-           
-        std::cout<< static_cast<int>(s_) << "||" << static_cast<int>(i_) << "||" << static_cast<int>(r_) << "||" << static_cast<int>(d_) << "||" << static_cast<int>(s_+i_+r_+d_)<< std::endl;
-
-        s=s_;
-        i=i_;
-        r=r_;
-        d=d_;
-    }
-}
-
-
-
-
-
-
-
-
