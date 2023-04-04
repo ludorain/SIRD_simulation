3c5a5b6be8d89639cda858e62bd936819ee4a125
nicoleLunedei <127126212+nicoleLunedei@users.noreply.github.com>
2023-04-04 13:16:32 +0200

    Update pandemic.hpp

--- SFML/pandemic.hpp
+++ SFML/pandemic.hpp
@@ -1,46 +1,59 @@
-// automa cellulare
+//automa cellulare 
 #ifndef PANDEMIC_HPP
 #define PANDEMIC_HPP
-#include <cassert>
-#include <iostream>
 #include <random>
-
+#include <iostream>
+#include <cassert>
 namespace life {
-enum Person {
-  Susceptible = 0,
-  Infected,
-  Recovered,
-  Dead
-}; // tipo enumeratore; tipo oggeto stato della cella
-
+ enum Person {Susceptible = 0 ,Infected , Recovered, Dead}; //tipo enumeratore; tipo oggeto stato della cella 
+ struct Probability 
+ {
+    float beta;//infettare un suscettibile
+    float alfa;//vaccinare un suscettibile
+    float gamma;//guarire un infetto
+    float omega;//morte di un infetto 
+ };
+ struct Count 
+ {
+    int s;
+    int i;
+    int r;
+    int d;
+ };
 // futura associazione di colore per ogni stato
-using Grid = std::vector<Person>; // rinominazione di una variabile scomoda
-class Pandemic {
-private:
-  // creazione della griglia
-  Grid pan_grid;
-  int pan_side; // il lato della griglia
+using Grid  = std::vector<Person>;//rinominazione di una variabile scomoda
+ class Pandemic
+{
+   private: 
+   //creazione della griglia 
+    Grid pan_grid;
+    int pan_side;//il lato della griglia
 
-public:
-  // Cotruisce una griglia bidimensionale (agent x agent)
-  Pandemic(int &);
-  void start(Pandemic &, int &);
-  // Accesso alla persona quindi al suo stato
-  // friend Person const& Person_cell(int r, int c) ;//solo lettura
-  // get a mutable reference to the Person at position (r,c)
-  Person &Person_cell(int r, int c); // anche scrittura
-  friend bool operator==(Pandemic const &left, Pandemic const &right);
-  friend bool operator!=(Pandemic const &left, Pandemic const &right);
-
-  int infected_neighbours(Pandemic &pandemic, int r, int c);
-  Pandemic evolve(Pandemic &current, float &beta, float &gamma, float &alfa1,
-                  float &alfa2, float &omega, float &s, float &i, float &r,
-                  float &d, int const &T, double const &t_m);
-
-  int side() const;
-  // void print(Pandemic const& p) ;
+   public:
+    //Cotruisce una griglia bidimensionale (agent x agent)
+    Pandemic(int&);
+    void start (Pandemic& , int&);
+    //Accesso alla persona quindi al suo stato  
+    Person const& Reading_cell(int r, int c) ;//solo lettura
+    // get a mutable reference to the Person at position (r,c)
+    Person& Writing_cell(int r, int c);//anche scrittura
+    friend bool operator==(Pandemic const& left, Pandemic const& right){
+        return left.pan_grid == right.pan_grid;
+    }
+    friend bool operator!=(Pandemic const& left, Pandemic const& right)
+    {
+        return left.pan_grid != right.pan_grid;
+    }
+    int infected_neighbours(Pandemic& pandemic, int r, int c);
+    Pandemic evolve(Pandemic& current, Probability& prob, Count& count); //ho messo le probabilità come reference per alleggerire l'allocazione in memoria
+    int const side ();
+    void check_number(Count& );
+    // Grid grid();
+    //void print(Pandemic& p, Probability& prob, Count& count,int& T);
 };
-} // namespace life
+}
+ 
+
+
 
 #endif
-
