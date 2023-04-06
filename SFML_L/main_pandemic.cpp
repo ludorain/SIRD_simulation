578f5132e09e72f7df444d2d95e115d793862cb7
Ludovica Rainero <ludovica.rainero@studio.unibo.it>
2023-04-06 23:31:34 +0200

    tailMap

---
+++ SFML_L/main_pandemic.cpp
@@ -0,0 +1,170 @@
+ #include <SFML/Graphics.hpp>
+//#include <SFML/Time.hpp>
+//#include <SFML/Clock.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+#include <iostream>
+#include "pandemic.hpp"
+//#include "button.hpp"
+//using namespace life;
+
+
+int main (){
+//Init simulation values
+class Pandemic ;
+//sicuramente c'è un modo per compattare righe 18-21
+//ora sono stanca e non mi viene in mente
+int l;
+std::cout<<"lengh";
+std::cin >> l;
+const int mapSize = l;
+//Input values
+life::Count count_;
+std::cout<<"s \n";
+std::cin >> count_.s;
+std::cout<<"i \n";
+std::cin >> count_.i;
+std::cout<<"r \n";
+std::cin >> count_.r; 
+std::cout<<"d \n";
+std::cin >> count_.d;
+
+int T;
+std::cout<<"Time \n";
+std::cin >> T;
+//Init map variables
+float gridSizeF = 680/mapSize;
+float dt = 0.f;
+sf::Clock dtClock;
+
+
+//Init window     
+sf::RenderWindow window(sf::VideoMode(1200, 700), "Pandemic evolution");
+
+//Create graphic elements
+sf::RectangleShape shape(sf::Vector2f(gridSizeF, gridSizeF));
+std::vector<std::vector <sf::RectangleShape>> tileMap;
+tileMap.resize(mapSize, std::vector<sf::RectangleShape>());
+
+//Creating simulation objects
+life::Pandemic population(l);
+life::Probability prob_={1,30,10,1};
+
+
+    /*Se volessimo mettere un bottone qui ci andrebbe
+    l'if(botton is pressed)*/
+
+//Set population (Pandemic object) with initial values
+population = life::Pandemic::start(population, count_.i); 
+//std::cout<< population << std::endl;
+
+//Initializing map with population initial values
+for(size_t x=0; x< mapSize; x++)
+    {   
+        tileMap[x].resize(mapSize, sf::RectangleShape());
+
+        for(size_t y=0; y< mapSize; y++)
+        {   
+            tileMap[x][y].setSize(sf::Vector2f(gridSizeF, gridSizeF));
+            tileMap[x][y].setOutlineThickness(1.f);
+            tileMap[x][y].setOutlineColor(sf::Color::White);
+            tileMap[x][y].setPosition(x*gridSizeF, y*gridSizeF);
+
+            if (population.Reading_cell(x,y) == life::Person::Susceptible )
+            {
+            tileMap[x][y].setFillColor(sf::Color::Blue);
+            } else if(population.Reading_cell(x,y) == life::Person::Infected)
+                {
+                tileMap[x][y].setFillColor(sf::Color::Red);
+                }
+        }
+    }
+
+    while (window.isOpen())
+    {
+        for(size_t x=0; x< mapSize; x++)        
+            {   for(size_t y=0; y< mapSize; y++)  
+                { 
+                    window.draw(tileMap[x][y]);
+                }
+            }
+        //update dt
+        dt = dtClock.restart().asSeconds();
+        window.setView(window.getDefaultView());
+
+        //Events
+        sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed) {
+                window.close(); 
+                }
+            }
+
+        window.clear();        
+        window.draw(shape);
+
+        //forse ci va un primo draw tileMap qui
+
+        for (int j = 0; j != T ; j++)
+        {
+            population = life::Pandemic::evolve(population, prob_, count_);
+            
+            // idealmente metodo draw_map()
+            for(size_t x=0; x< mapSize; x++)
+            {
+                for(size_t y=0; y< mapSize; y++)
+                {   
+                    if (population.Reading_cell(x,y) == life::Person::Susceptible )
+                    {tileMap[x][y].setFillColor(sf::Color::Blue);}
+
+                    if(population.Reading_cell(x,y) == life::Person::Infected)
+                    {tileMap[x][y].setFillColor(sf::Color::Red);}
+
+                    if(population.Reading_cell(x,y) == life::Person::Recovered)
+                        {tileMap[x][y].setFillColor(sf::Color::Green);}
+
+                    if(population.Reading_cell(x,y) == life::Person::Dead)
+                        {tileMap[x][y].setFillColor(sf::Color::Black);}
+                }
+            }  
+
+            for(size_t x=0; x< mapSize; x++)        
+            {   for(size_t y=0; y< mapSize; y++)  
+                { 
+                    window.draw(tileMap[x][y]);
+                }
+            }
+        window.setView(window.getDefaultView()); //this is gonna reset the view at the end of the drawing
+       
+        window.display();
+        //////////////////////////////////////////////
+        sf::Clock clock;
+        sf::Time elapsed = clock.restart();
+        }
+        
+
+       
+    }
+
+    //creazione dei bottoni 
+    
+    //creazione della griglia, con cambiamento delle celle coi 4 colori associati (nero=morti, blu=susciettibili, rosso=infettati, verde=guariti): display.hpp
+
+    
+
+    /*
+    population.Writing_cell(4,3) = life::Person::Infected;
+    population.Writing_cell(3,3) = life::Person::Infected;
+    population.Writing_cell(5,3) = life::Person::Infected;
+    population.Writing_cell(5,2) = life::Person::Infected;
+    population.Writing_cell(3,2) = life::Person::Infected;
+    population.Writing_cell(5,5) = life::Person::Infected;
+    */
+    
+    
+
+    return 0;
+  
+}
---
+++ SFML_L/pandemic.cpp
@@ -0,0 +1,178 @@
+#include "pandemic.hpp"
+#include <cassert>
+#include <iostream>
+#include <random>
+
+using namespace life;
+
+Pandemic::Pandemic(int& N) : 
+  pan_side {N},
+  pan_grid (N * N)     //, Person::Susceptible)
+{} 
+
+
+// inizializza all'inizio 
+
+ /*bool operator==(Pandemic const& left, Pandemic const& right)//utile per i testcase; cosa utile per orale=> qua si sfrutta l'operatore == dei vector, per evitare di scorrere cella per cella e verificare che il contenuto sia uguale 
+{
+   return left.Pandemic::grid() == right.Pandemic::grid(); 
+}
+ bool operator!=(Pandemic const& left, Pandemic const& right)
+{
+  return left.pan_grid != right.pan_grid;
+}*/
+
+
+Person const& Pandemic::Reading_cell(int r, int c)  //accesso tramite gli indici r e c; correggi per forma toroidale 
+{ 
+  int r_ = (r + pan_side) % pan_side;//qua creo la struttura toroidale 
+  int c_= (c + pan_side ) % pan_side;
+  //assert(r_>= 0 && r_< pan_side && c_>= 0 && c_< pan_side);
+  int index = (r_* pan_side) + c_;//passaggio da 2 indici ad un indice per la griglia 
+  //assert(index >= 0 && index < pan_grid.size());
+  return pan_grid[index];
+
+} 
+
+
+Person& Pandemic::Writing_cell(int r, int c)//è necessario prima accedere alla riga r-esima e poi accedere all'elemento c-esimo
+{ 
+  //int l = grid.side();
+  int r_ = (r + pan_side ) % pan_side;//qua creo la struttura toroidale-> arrotolo il tovagliolo 
+  int c_= (c + pan_side ) % pan_side;//-> cucio le estremità
+  //assert(r_>= 0 && r_< pan_side && c_>= 0 && c_< pan_side);
+  int index = (r_* pan_side) + c_;//passaggio da 2 indici ad un indice per la griglia; nonn l'ho ben capito  
+  //assert(index >= 0 && index < pan_grid.size());
+  return pan_grid[index];   
+}//da controllare con assert nei TEST CASE
+
+int const Pandemic::side (){
+  return  pan_side;
+}
+
+Grid Pandemic::grid(){
+  return pan_grid;
+}
+
+
+Pandemic Pandemic::start(Pandemic& clear,int& inf )       //iniettare gli infettati iniziali
+{
+  int l = clear.pan_side;
+  std::default_random_engine eng{std::random_device{}()};
+  std::uniform_int_distribution<int> dist{0, l - 1};
+  //copia dell'oggetto
+  Pandemic set(l);
+
+  for (int j=0;j < inf; j++)
+  {
+    int k = dist(eng);
+    int h = dist(eng);
+    
+    while (set.Reading_cell(k,h) == Person::Infected)
+    {
+      k++;
+      h++;
+    }
+
+    set.Writing_cell(k,h) = Person::Infected;
+  }
+  return set;
+}
+
+
+int Pandemic::infected_neighbours(Pandemic& pandemic, int r, int c)
+{
+  int contacts = 0;
+  for (int i : {-1, 0, 1}) //vado su e giù nelle righe 
+  {
+    for (int j : {-1, 0, 1}) //vado a destra e a sinistra nelle colonne 
+    {
+      if (pandemic.Reading_cell( r+i, c+j) == Person::Infected) {
+        contacts++;
+      }
+    }
+  }
+
+  return contacts;//n numero dei contatti infetti}
+}
+
+
+
+Pandemic Pandemic::evolve(Pandemic& current, Probability& prob, Count& count) //ho messo le probabilità come reference per alleggerire l'allocazione in memoria
+{
+  int l = current.pan_side; //lato della griglia dell'oggetto current
+  Pandemic next (l); // uso il costruttore di pandemic che riechiede come parametro solo il lato della griglia ovvero l 
+
+  next=current;
+
+  assert ((l*l) == (count.s + count.i + count.r + count.d)); //l'area della griglia deve essere uguale alla popolazione  
+  assert ((prob.alfa + prob.beta) < 100); //verifica della normalizzazione delle probabilità
+
+  std::default_random_engine eng{std::random_device{}()};//settaggio del seme random della libreria std
+  std::uniform_real_distribution <float> dis(0.0, 1.0); //dichiarazione della distribuzione uniforme di numeri float 
+
+  for (int r = 0 ; r < l ; r++ )    // loop sulle righe   //NOTA DELLA SOFIA: la sofi crede che qui ci vada < e non <= perchè parte da 0, quindi fa l cicli quando arriva a l-1
+  {
+    for (int c = 0 ; c < l ; c++)  // loop sulle celle di ogni riga
+    {
+      if (current.Reading_cell(r,c) == Person::Susceptible )  //suscettibili
+      {
+        float p1 = dis (eng);//da verificare per estrazione tra 0 e 1
+        double b = (prob.beta/100)*current.infected_neighbours(current, r, c);// calcola la probabilità specifica per la cella 
+           
+           if (prob.alfa != 0 && p1 <= (prob.alfa/100) )
+            {
+              next.Writing_cell(r,c) = Person::Recovered;       //riassegnazione dello stato nella stessa posizione ma nella griglia successiva 
+              count.r++;
+              count.s--;
+
+            } else  if ( p1 <= ((prob.alfa/100)+ b)) {
+
+              next.Writing_cell(r,c) = Person::Infected;
+              count.i++; 
+              count.s--; 
+                  
+            } else {
+              // qui rimane suscettibile -> non serve sto else lo metto solo per capire
+            }
+
+      } else if (current.Reading_cell(r,c) == Person::Infected) {                   //infetti
+        //generazione di un numero random da 0 a 1 
+        //std::this_thread::sleep_for(std::chrono::(t_m));//ipotetico tempo di quarantena con la malattia  
+        double p2 = dis(eng);
+        if (p2<=(prob.gamma/100))                         //probabilità di guarire
+        {
+          next.Writing_cell(r,c) = Person::Recovered;
+          count.r++;
+          count.i--;                                      
+        } else {
+          next.Writing_cell(r,c) = Person::Dead; //probabilità di morire 
+          count.i--;
+          count.d++;              //sarebbe carino far passare del tempo prima di fare guarire o morire
+        } 
+
+      }
+    }
+  }
+
+  return next;//restituisce una nuova griglia con i nuovi stati
+}
+
+
+
+
+
+
+
+
+
+/*void Pandemic::check_number(Count& c_start, int& n)
+{ 
+  int A = pan_side * pan_side;
+  c_start.d = n ;
+  c_start.r = 
+  
+
+}*/
+ 
+
