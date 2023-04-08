a011eab20a6a371af118e86d3ed94d823464ab0e
Sofia <127309031+SofiZala@users.noreply.github.com>
2023-04-08 03:51:24 +0200

    Update pandemic_test.cpp

--- SFML_L/pandemic_test.cpp
+++ SFML_L/pandemic_test.cpp
@@ -1,50 +1,113 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <random> 
+
 #include "doctest.h"
+#include "parameters.hpp"
 #include "pandemic.hpp"
 
-TEST_CASE("Testing state cell")
-{  
-  std::default_random_engine eng{std::random_device{}()};
-  std::uniform_int_distribution<int> dist{7, 100 - 1};
-  int l = dist(eng);
-   
-  Pandemic evolving(l);
+#include <vector>
+#include <random> 
+
+
+TEST_CASE("Setters class Parameters.") {
+
+  Parameters ps;
+  Parameters ps1;
+  
+  SUBCASE("Calling setters with correct values & checking getters.")
+ 
+  CHECK(ps.set_Beta(0.1) == true);
+  CHECK(ps.set_Beta(0.998) == true);
+  CHECK(ps.set_Beta(0.7) == true);
+  
+  CHECK(ps.set_Gamma(0.1) == true);
+  CHECK(ps.set_Gamma(0.3) == true);
+  CHECK(ps.set_Gamma(0.5) == true);
+
+  CHECK(ps.set_Mu(0.1) == true);
+  CHECK(ps.set_Mu(0.3) == true);
+  CHECK(ps.set_Mu(0.5) == true);
+
+  CHECK(ps.get_Alfa() == 0.001);
+  CHECK(ps.get_Beta() == 0.7);
+  CHECK(ps.get_Gamma() == 0.5);
+  CHECK(ps.get_Mu() == 0.5);
+  
+
+  SUBCASE("Checking default constructor.")
+  
+  CHECK(ps1.get_Alfa() == 0.001);
+  CHECK(ps1.get_Beta() == 0.85);
+  CHECK(ps1.get_Gamma() == 0.2);
+  CHECK(ps1.get_Mu() == 0.2);
+
+
+  SUBCASE("Calling setters with wrong values.")
+ 
+  CHECK(ps.set_Beta(0) == false);
+  CHECK(ps.set_Beta(0.999) == false);
+  CHECK(ps.set_Beta(-0.2) == false);
+
+  CHECK(ps.set_Gamma(0) == false);
+  CHECK(ps.set_Gamma(-0.2) == false);
+  CHECK(ps.set_Gamma(1) == false);
+
+  CHECK(ps.set_Mu(0) == false);
+  CHECK(ps.set_Mu(-0.2) == false);
+  CHECK(ps.set_Mu(1) == false);
+
+}
+
+
+
+/*
+TEST_CASE( "Testing state cell") {
+
+  Parameters ps;
+  Pandemic evolving;
 
   SUBCASE ("Reading and changing person's state");
 
-  Probability prob_test  = {2,35,8,3}; 
-  Count count_test;
-  CHECK(evolving.Reading_cell(1,1) == Person::Susceptible); // check iniziale
+  std::default_random_engine eng{std::random_device{}()};
+  std::uniform_int_distribution<int> dist{10, 50};
+  int l = dist(eng);
+
+  evolving.set_Side(l); 
+
+  
+  CHECK(evolving.Reading_cell(1,1) == Person::Susceptible); 
   evolving.Writing_cell(1,1) = Infected;
   CHECK(evolving.Reading_cell(1,1) == Person::Infected);
-  evolving.Writing_cell(1,3) = Person::Recovered;//provax
+
+  evolving.Writing_cell(1,3) = Person::Recovered;
   CHECK(evolving.Reading_cell(1,3) == Person::Recovered); 
+
   evolving.Writing_cell(5,1) = Person::Dead;
   CHECK(evolving.Reading_cell(5,1) == Person::Dead);
   
+
   SUBCASE("Testing evolution");
 
   l = 11;
   evolving.Writing_cell(4,6) = Person::Infected;
   evolving.Writing_cell(3,5) = Person::Recovered;
   evolving.Writing_cell(5,3) = Person::Dead;
-  evolving.Writing_cell(6,2) = Person::Recovered;
 
-  evolving = Pandemic::evolve(evolving, prob_test,count_test);
+
+  evolving = Pandemic::evolve(evolving, ps);
 
   CHECK(evolving.Reading_cell(4,6) != Person::Susceptible);
+
   CHECK(evolving.Reading_cell(3,5) != Person::Susceptible);
+  CHECK(evolving.Reading_cell(3,5) != Person::Infected);
   CHECK(evolving.Reading_cell(3,5) != Person::Dead);
+
   CHECK(evolving.Reading_cell(5,3) != Person::Susceptible);
   CHECK(evolving.Reading_cell(5,3) != Person::Recovered);
-  CHECK(evolving.Reading_cell(6,2) != Person::Susceptible);
-  CHECK(evolving.Reading_cell(3,5) != Person::Infected);
-  CHECK(evolving.Reading_cell(4,4) != Person::Dead);
+  CHECK(evolving.Reading_cell(5,3) != Person::Infected);
 
+} */
 
-}
-
+/*
 
 TEST_CASE ("Testing initial situation")
 { // testing the start member function
@@ -53,7 +116,8 @@ SUBCASE("Setting the initial grid by calling the member function start()");
   
   // Creating a pandemic object
   int l = 50;
-  Pandemic first(l);
+  Pandemic first;
+  first.set_Side(l);
   int inf = 90;
   //calling start()
   first = Pandemic::start(first, inf);
@@ -74,9 +138,10 @@ SUBCASE("Setting the initial grid by calling the member function start()");
   int n = 55;
   life::Pandemic::check_number(n,lato);
   CHECK(lato == 50 && n == 52);*/
-} 
+  
+//} 
 
-
+/*
 TEST_CASE("Testing the counting of the infected neighbours")
 {
   std::default_random_engine eng{std::random_device{}()};
@@ -99,3 +164,7 @@ TEST_CASE("Testing the counting of the infected neighbours")
   CHECK(neighbours.Reading_cell(3,2)<=8);
   CHECK(neighbours.Reading_cell(2,3)<=8);
 }
+
+
+
+*/
