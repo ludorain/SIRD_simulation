e4d3ed5734f596ef4c39668b57251add49798eef
Sofia <127309031+SofiZala@users.noreply.github.com>
2023-04-08 05:13:35 +0200

    Update pandemic_test.cpp

--- SFML_L/pandemic_test.cpp
+++ SFML_L/pandemic_test.cpp
@@ -8,73 +8,66 @@
 #include <random> 
 
 
-TEST_CASE("Setters class Parameters.") {
+TEST_CASE("Setters & getters class Parameters.") {
 
   Parameters ps;
   Parameters ps1;
-  
+
+
   SUBCASE("Calling setters with correct values & checking getters.")
- 
-  CHECK(ps.set_Beta(0.1) == true);
-  CHECK(ps.set_Beta(0.998) == true);
-  CHECK(ps.set_Beta(0.7) == true);
   
-  CHECK(ps.set_Gamma(0.1) == true);
-  CHECK(ps.set_Gamma(0.3) == true);
-  CHECK(ps.set_Gamma(0.5) == true);
+  ps.set_Beta(0.2);
+  CHECK(ps.get_Beta() == doctest::Approx(0.2));
 
-  CHECK(ps.set_Mu(0.1) == true);
-  CHECK(ps.set_Mu(0.3) == true);
-  CHECK(ps.set_Mu(0.5) == true);
+  ps.set_Gamma(0.1);
+  CHECK(ps.get_Gamma() == doctest::Approx(0.1));
 
-  CHECK(ps.get_Alfa() == 0.001);
-  CHECK(ps.get_Beta() == 0.7);
-  CHECK(ps.get_Gamma() == 0.5);
-  CHECK(ps.get_Mu() == 0.5);
+  ps.set_Mu(0.1);
+  CHECK(ps.get_Mu() == doctest::Approx(0.1));
+  
   
 
   SUBCASE("Checking default constructor.")
   
-  CHECK(ps1.get_Alfa() == 0.001);
-  CHECK(ps1.get_Beta() == 0.85);
-  CHECK(ps1.get_Gamma() == 0.2);
-  CHECK(ps1.get_Mu() == 0.2);
-
-
-  SUBCASE("Calling setters with wrong values.")
- 
-  CHECK(ps.set_Beta(0) == false);
-  CHECK(ps.set_Beta(0.999) == false);
-  CHECK(ps.set_Beta(-0.2) == false);
-
-  CHECK(ps.set_Gamma(0) == false);
-  CHECK(ps.set_Gamma(-0.2) == false);
-  CHECK(ps.set_Gamma(1) == false);
-
-  CHECK(ps.set_Mu(0) == false);
-  CHECK(ps.set_Mu(-0.2) == false);
-  CHECK(ps.set_Mu(1) == false);
-
+  CHECK(ps1.get_Alfa() == doctest::Approx(0.001));
+  CHECK(ps1.get_Beta() == doctest::Approx(0.85));
+  CHECK(ps1.get_Gamma() == doctest::Approx(0.2));
+  CHECK(ps1.get_Mu() == doctest::Approx(0.2));
+  
 }
 
 
+TEST_CASE ("Testing class Pandemic.") {
 
-/*
-TEST_CASE( "Testing state cell") {
-
-  Parameters ps;
+  Pandemic pan;
+  Pandemic pan1;
   Pandemic evolving;
-
-  SUBCASE ("Reading and changing person's state");
-
-  std::default_random_engine eng{std::random_device{}()};
-  std::uniform_int_distribution<int> dist{10, 50};
-  int l = dist(eng);
-
-  evolving.set_Side(l); 
+  Pandemic evolving2;
+  Pandemic neighbours;
+  Parameters ps;
 
   
-  CHECK(evolving.Reading_cell(1,1) == Person::Susceptible); 
+  SUBCASE("Calling setters with correct values & checking getters.")
+
+  pan.set_I(100);
+  CHECK(pan.get_I()==100);
+
+  pan.set_Side(30);
+  CHECK(pan.get_I()==30);
+  
+  
+
+  SUBCASE("Checking default constructor.")
+  
+  CHECK(pan1.get_I() == 18);
+  CHECK(pan1.get_Side() == 20);
+
+
+
+  SUBCASE("Reading and changing person's state") 
+
+  CHECK(evolving.Reading_cell(1,1) == Person::Susceptible);
+
   evolving.Writing_cell(1,1) = Infected;
   CHECK(evolving.Reading_cell(1,1) == Person::Infected);
 
@@ -83,15 +76,14 @@ TEST_CASE( "Testing state cell") {
 
   evolving.Writing_cell(5,1) = Person::Dead;
   CHECK(evolving.Reading_cell(5,1) == Person::Dead);
-  
 
-  SUBCASE("Testing evolution");
 
-  l = 11;
+
+  SUBCASE("Testing evolution")
+
   evolving.Writing_cell(4,6) = Person::Infected;
   evolving.Writing_cell(3,5) = Person::Recovered;
   evolving.Writing_cell(5,3) = Person::Dead;
-
 
   evolving = Pandemic::evolve(evolving, ps);
 
@@ -105,59 +97,59 @@ TEST_CASE( "Testing state cell") {
   CHECK(evolving.Reading_cell(5,3) != Person::Recovered);
   CHECK(evolving.Reading_cell(5,3) != Person::Infected);
 
-} */
 
-/*
 
-TEST_CASE ("Testing initial situation")
-{ // testing the start member function
-
-SUBCASE("Setting the initial grid by calling the member function start()");
+  SUBCASE("Setting the initial grid by calling the member function start().")
   
-  // Creating a pandemic object
   int l = 50;
-  Pandemic first;
-  first.set_Side(l);
-  int inf = 90;
-  //calling start()
-  first = Pandemic::start(first, inf);
+  evolving2.set_Side(l);
+  int infected = 90;
+
+  evolving2 = Pandemic::start(evolving2, infected);
   
   int check = 0;
 
-    for (int r = 0 ; r < l ; r++ ){   
-   
-       for (int c = 0 ; c < l ; c++){
+  for (int r = 0 ; r < l ; r++ ) {   
+  
+    for (int c = 0 ; c < l ; c++) {
 
-        if (first.Reading_cell(r,c) == Person::Infected) check++;
-       }
+      if (evolving2.Reading_cell(r,c) == Person::Infected) 
+      {check++;}
     }
+  }
   
-  CHECK(check == inf);
+  CHECK(check == infected);
 
-  /*int lato = 51;
-  int n = 55;
-  life::Pandemic::check_number(n,lato);
-  CHECK(lato == 50 && n == 52);*/
-  
-//} 
+
+
+
+  SUBCASE("Testing number of infected neighbours.")
+
+  for (int i : {-1,0,1}) {
+    for (int j : {-1,0,1}) {
+      neighbours.Writing_cell(4+i,6+j) = Person::Infected;
+    }
+  }
+  //non sho grandi idee s come scrivere il test dei vicini
+
+
+
+}
+
+
+
+
 
 /*
-TEST_CASE("Testing the counting of the infected neighbours")
 {
-  std::default_random_engine eng{std::random_device{}()};
-  std::uniform_int_distribution<int> dist{7, 100 - 1};
-  int l = dist(eng);
-
-  Pandemic neighbours(l);
-  Probability pneighbours  = {2,35,8,3};  
-  Count cneighbours;
+  
   neighbours.Writing_cell(4,6) = Person::Infected;
   neighbours.Writing_cell(2,5) = Person::Susceptible;
   neighbours.Writing_cell(3,2) = Person::Infected;
   neighbours.Writing_cell(2,3) = Person::Susceptible;
   //evolving states 5 times
   for (int i = 0; i < 5;i++ ){
-    Pandemic::evolve(neighbours,pneighbours,cneighbours);
+    Pandemic::evolve(neighbours,ps);
   }
   CHECK(neighbours.Reading_cell(4,6)<=8);
   CHECK(neighbours.Reading_cell(2,5)<=8);
@@ -168,3 +160,4 @@ TEST_CASE("Testing the counting of the infected neighbours")
 
 
 */
+
